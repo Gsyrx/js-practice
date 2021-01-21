b41fc198a72bee933a0f5f1a1303f5ef2167dc52
Gaurav Kumar <gauravsyr10@gmail.com>
2021-01-21 19:44:43 +0530

    worked with script file

---
+++ quote-generator/script.js
@@ -0,0 +1,24 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// Get Quote from API
+async function getQuote() {
+  const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+  const apiURL =
+    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyURL + apiURL);
+    const data = await response.json();
+    // console.log(data);
+    authorText.innerText = data.quoteAuthor;
+    quoteText.innerText = data.quoteText;
+  } catch (error) {
+    console.log('No Quote', error);
+  }
+}
+
+// On loading
+getQuote();
